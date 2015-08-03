89f99d7a291a0a142573d3c7e36094df6382ad66
André Leão <andre.sp.leao@gmail.com>
2015-08-02 23:37:25 -0300

    Inclusão dos links do sistema  no sidebar.

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -14,12 +14,42 @@
                 </div>
                 <!-- /input-group -->
             </li>
-            
+
             <li>
-                <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Relatórios<span class="fa arrow"></span></a>
+                <a href="#"><i class="fa  fa-user  fa-fw"></i> Cliente <span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
                     <li>
-                        <a href="#">Relatório 1</a>
+                        <a href="/customer/add">Adicionar</a>
+                    </li>
+                </ul>
+                <!-- /.nav-second-level -->
+            </li>
+
+             <li>
+                <a href="#"><i class="fa  fa-user  fa-fw"></i> Fornecedor <span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="/supplier/add">Adicionar</a>
+                    </li>
+                </ul>
+                <!-- /.nav-second-level -->
+            </li>
+            
+            <li>
+                <a href="#"><i class="fa  fa-user  fa-fw"></i> Funcionario <span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="/employee/add">Adicionar</a>
+                    </li>
+                </ul>
+                <!-- /.nav-second-level -->
+            </li>
+            
+             <li>
+                <a href="#"><i class="fa  fa-dropbox   fa-fw"></i> Estoque <span class="fa arrow"></span></a>
+                <ul class="nav nav-second-level">
+                    <li>
+                        <a href="/stock/add">Adicionar</a>
                     </li>
                 </ul>
                 <!-- /.nav-second-level -->
@@ -35,46 +65,22 @@
                 </ul>
                 <!-- /.nav-second-level -->
             </li>
-            
+
             <li>
-                <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
-            </li>
-            <li>
-                <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
-            </li>
-            <li>
-                <a href="#"><i class="fa fa-wrench fa-fw"></i> UI Elements<span class="fa arrow"></span></a>
+                <a href="#"><i class="fa  fa-dropbox   fa-fw"></i> Compra <span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
                     <li>
-                        <a href="panels-wells.html">Panels and Wells</a>
-                    </li>
-                    <li>
-                        <a href="buttons.html">Buttons</a>
-                    </li>
-                    <li>
-                        <a href="notifications.html">Notifications</a>
-                    </li>
-                    <li>
-                        <a href="typography.html">Typography</a>
-                    </li>
-                    <li>
-                        <a href="icons.html"> Icons</a>
-                    </li>
-                    <li>
-                        <a href="grid.html">Grid</a>
+                        <a href="/purchase/add">Nova</a>
                     </li>
                 </ul>
                 <!-- /.nav-second-level -->
             </li>
             
             <li>
-                <a href="#"><i class="fa fa-files-o fa-fw"></i> Sample Pages<span class="fa arrow"></span></a>
+                <a href="#"><i class="fa  fa-dropbox   fa-fw"></i> Venda <span class="fa arrow"></span></a>
                 <ul class="nav nav-second-level">
                     <li>
-                        <a href="blank.html">Blank Page</a>
-                    </li>
-                    <li>
-                        <a href="login.html">Login Page</a>
+                        <a href="/sale/add">Nova</a>
                     </li>
                 </ul>
                 <!-- /.nav-second-level -->
